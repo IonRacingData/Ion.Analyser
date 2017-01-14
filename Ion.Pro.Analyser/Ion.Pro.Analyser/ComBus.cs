d3a65977bf6a2816fcdc119b36940e1c93eb125c
Nicolas Fløysvik <nico-floysvik@hotmail.com>
2017-01-14 14:08:16 +0100

    Started adding ComBus

---
+++ Ion.Pro.Analyser/Ion.Pro.Analyser/ComBus.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ion.Pro.Analyser
+{
+    public class ComBus
+    {
+
+        public List<ComBusClient> ConnectedClients { get; private set; } = new List<ComBusClient>();
+        static ComBus Default { get; set; }
+
+        private ComBus()
+        {
+
+        }
+
+        public static ComBus GetDefault()
+        {
+            if (Default == null)
+            {
+                Default = new ComBus();
+            }
+            return Default;
+        }
+
+        public void RegisterClient(ComBusClient client)
+        {
+            ConnectedClients.Add(client);
+        }
+
+
+    }
+
+    public class ComBusClient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class WebSocketComBusClient : ComBusClient
+    {
+        public TcpClient Client;
+
+
+    }
+}
