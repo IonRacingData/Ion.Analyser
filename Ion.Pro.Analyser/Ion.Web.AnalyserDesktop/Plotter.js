7127aea27af9b1c31245f4b6209e37cb9e4b4b35
unputdownable <stiantrondsen@hotmail.com>
2017-01-18 09:41:49 +0100

    fixed zoom

--- Ion.Pro.Analyser/Ion.Web.AnalyserDesktop/Plotter.js
+++ Ion.Pro.Analyser/Ion.Web.AnalyserDesktop/Plotter.js
@@ -2,7 +2,7 @@ var Plotter = (function () {
     function Plotter() {
         this.movePoint = { x: 50, y: 50 };
         this.scalePoint = { x: 1, y: 1 };
-        this.zoomSpeed = 500;
+        this.zoomSpeed = 1.2;
     }
     Plotter.prototype.generatePlot = function (data) {
         var _this = this;
@@ -15,7 +15,7 @@ var Plotter = (function () {
         this.canvas.addEventListener("mousemove", function (e) {
             if (_this.dragging) {
                 _this.movePoint = { x: e.layerX + _this.mouseMod.x, y: (_this.canvas.height - e.layerY) + _this.mouseMod.y };
-                console.log(_this.movePoint);
+                console.log(_this.getRelative({ x: e.layerX, y: e.layerY }));
                 _this.draw();
             }
         });
@@ -29,23 +29,23 @@ var Plotter = (function () {
         return this.canvas;
     };
     Plotter.prototype.zoom = function (e) {
-        var zoomX = this.scalePoint.x - e.deltaY / this.zoomSpeed;
-        var zoomY = this.scalePoint.y - e.deltaY / this.zoomSpeed;
-        if (zoomX > 0) {
-            this.scalePoint.x = zoomX;
-        }
-        if (zoomY > 0) {
-            this.scalePoint.y = zoomY;
-        }
-        if (e.deltaY > 0) {
-            this.movePoint.x += (e.layerX - this.movePoint.x) / 1000;
-            this.movePoint.y += ((this.canvas.height - e.layerY) - this.movePoint.y) / 1000;
+        var mousePoint = this.getMousePoint(e);
+        var curRel = this.getRelative(mousePoint);
+        if (e.deltaY < 0) {
+            this.scalePoint.x *= this.zoomSpeed;
+            this.scalePoint.y *= this.zoomSpeed;
         }
         else {
-            this.movePoint.x -= (e.layerX - this.movePoint.x) / 1000;
-            this.movePoint.y -= ((this.canvas.height - e.layerY) - this.movePoint.y) / 1000;
+            this.scalePoint.x /= this.zoomSpeed;
+            this.scalePoint.y /= this.zoomSpeed;
         }
+        var newRel = this.getRelative(mousePoint);
+        var move = { x: (newRel.x - curRel.x) * this.scalePoint.x, y: (newRel.y - curRel.y) * this.scalePoint.y };
+        this.movePoint = { x: this.movePoint.x + move.x, y: this.movePoint.y + move.y };
         this.draw();
+    };
+    Plotter.prototype.getMousePoint = function (e) {
+        return { x: e.layerX, y: e.layerY };
     };
     Plotter.prototype.draw = function () {
         var ctx = this.canvas.getContext("2d");
@@ -55,24 +55,51 @@ var Plotter = (function () {
         var lastPoint;
         for (var i = 0; i < this.data.length; i++) {
             var point = this.transform(this.createPoint(this.data[i]));
-            if (point.x > 0 && point.x < this.canvas.width) {
+            if (point.x > 0) {
                 ctx.moveTo(point.x, point.y);
                 if (i > 0) {
                     ctx.lineTo(lastPoint.x, lastPoint.y);
                 }
+                if (point.x > this.canvas.width) {
+                    break;
+                }
             }
             lastPoint = point;
         }
+        var origo = this.transform({ x: 0, y: 0 });
         // x-axis
-        ctx.moveTo(0, this.canvas.height - this.movePoint.y);
-        ctx.lineTo(this.canvas.width, this.canvas.height - this.movePoint.y);
+        ctx.moveTo(0, origo.y);
+        ctx.lineTo(this.canvas.width, origo.y);
         // y-axis
-        ctx.moveTo(this.movePoint.x, 0);
-        ctx.lineTo(this.movePoint.x, this.canvas.height);
+        ctx.moveTo(origo.x, 0);
+        ctx.lineTo(origo.x, this.canvas.height);
+        //var relWidth = this.canvas.width / this.scalePoint.x;
+        //var relHeight = this.canvas.height / this.scalePoint.y;
+        for (var i = 0; i < this.canvas.width; i++) {
+            var num = this.getRelative({ x: i, y: origo.y }).x;
+            num = Math.round(num);
+            if (num % 10 == 0) {
+                ctx.fillText(num.toString(), i, origo.y + 10);
+            }
+        }
+        var steps = 50;
+        /*
+        for (var i = -steps; i < this.canvas.width; i += steps) {
+            var transformer = this.getRelative({ x: i + this.movePoint.x % steps, y: origo.y });
+            ctx.fillText(transformer.x.toFixed(2), i + this.movePoint.x % steps, origo.y + 10);
+        }
+        */
+        for (var i = 0; i < this.canvas.width; i += 100) {
+        }
         ctx.stroke();
     };
     Plotter.prototype.createPoint = function (data) {
-        return { x: data.TimeStamp / 10, y: data.Value };
+        return { x: data.TimeStamp, y: data.Value };
+    };
+    Plotter.prototype.getRelative = function (p) {
+        var moved = { x: p.x - this.movePoint.x, y: this.canvas.height - p.y - this.movePoint.y };
+        var scaled = { x: moved.x / this.scalePoint.x, y: moved.y / this.scalePoint.y };
+        return scaled;
     };
     Plotter.prototype.transform = function (p) {
         var scaled = { x: p.x * this.scalePoint.x, y: p.y * this.scalePoint.y };
--- Ion.Pro.Analyser/Ion.Web.AnalyserDesktop/Plotter.ts
+++ Ion.Pro.Analyser/Ion.Web.AnalyserDesktop/Plotter.ts
@@ -10,7 +10,7 @@ class Plotter {
     scalePoint: Point = { x: 1, y: 1 };
     mouseMod: Point;    
     dragging: boolean;    
-    zoomSpeed: number = 500;
+    zoomSpeed: number = 1.2;
     
     generatePlot(data: ISensorPackage[]): HTMLCanvasElement {
         this.canvas = document.createElement("canvas");
@@ -23,7 +23,7 @@ class Plotter {
         this.canvas.addEventListener("mousemove", (e: MouseEvent) => {            
             if (this.dragging) {
                 this.movePoint = { x: e.layerX + this.mouseMod.x, y: (this.canvas.height - e.layerY) + this.mouseMod.y };
-                console.log(this.movePoint);
+                console.log(this.getRelative({ x: e.layerX, y: e.layerY }));
                 this.draw();
             }
         });
@@ -41,28 +41,31 @@ class Plotter {
         return this.canvas;
     }
 
-    zoom(e: WheelEvent) {
-        var zoomX = this.scalePoint.x - e.deltaY / this.zoomSpeed;
-        var zoomY = this.scalePoint.y - e.deltaY / this.zoomSpeed;
-        if (zoomX > 0) {
-            this.scalePoint.x = zoomX;
-        }
-        if (zoomY > 0) {
-            this.scalePoint.y = zoomY;
-        }
 
-        if (e.deltaY > 0) {
-            this.movePoint.x += (e.layerX - this.movePoint.x) / 1000;
-            this.movePoint.y += ((this.canvas.height - e.layerY) - this.movePoint.y) / 1000;
+
+    zoom(e: WheelEvent) {                
+
+        var mousePoint = this.getMousePoint(e);
+        var curRel = this.getRelative(mousePoint);
+
+        if (e.deltaY < 0) {
+            this.scalePoint.x *= this.zoomSpeed;
+            this.scalePoint.y *= this.zoomSpeed;
         }
         else {
-            this.movePoint.x -= (e.layerX - this.movePoint.x) / 1000;
-            this.movePoint.y -= ((this.canvas.height - e.layerY) - this.movePoint.y) / 1000;
+            this.scalePoint.x /= this.zoomSpeed;
+            this.scalePoint.y /= this.zoomSpeed;
         }
-         
-        
+        var newRel = this.getRelative(mousePoint);
+
+        var move: Point = { x: (newRel.x - curRel.x) * this.scalePoint.x, y: (newRel.y - curRel.y) * this.scalePoint.y };
+        this.movePoint = { x: this.movePoint.x + move.x, y: this.movePoint.y + move.y };
         this.draw();        
     }
+
+    getMousePoint(e: MouseEvent): Point {
+        return { x: e.layerX, y: e.layerY };
+    } 
 
     draw() {                
         var ctx = this.canvas.getContext("2d");
@@ -72,28 +75,61 @@ class Plotter {
         var lastPoint: Point;
         for (var i = 0; i < this.data.length; i++) {
             var point = this.transform(this.createPoint(this.data[i]));
-            if (point.x > 0 && point.x < this.canvas.width) {
+            if (point.x > 0) {
                 ctx.moveTo(point.x, point.y);
                 if (i > 0) {
                     ctx.lineTo(lastPoint.x, lastPoint.y);
                 }
+                if (point.x > this.canvas.width) {
+                    break;
+                }
             }
             lastPoint = point;       
         }
+
+        var origo = this.transform({ x: 0, y: 0 });
         // x-axis
-        ctx.moveTo(0, this.canvas.height - this.movePoint.y);
-        ctx.lineTo(this.canvas.width, this.canvas.height - this.movePoint.y);        
+        ctx.moveTo(0, origo.y);
+        ctx.lineTo(this.canvas.width, origo.y);        
 
         // y-axis
-        ctx.moveTo(this.movePoint.x, 0);
-        ctx.lineTo(this.movePoint.x, this.canvas.height);
+        ctx.moveTo(origo.x, 0);
+        ctx.lineTo(origo.x, this.canvas.height);
 
+        //var relWidth = this.canvas.width / this.scalePoint.x;
+        //var relHeight = this.canvas.height / this.scalePoint.y;
+
+        for (var i = 0; i < this.canvas.width; i++) {
+            var num = this.getRelative({ x: i, y: origo.y }).x;
+            num = Math.round(num);            
+            if (num % 10 == 0) {
+                ctx.fillText(num.toString(), i, origo.y + 10);                
+            }
+        }
+        
+        var steps = 50;
+        /*
+        for (var i = -steps; i < this.canvas.width; i += steps) {
+            var transformer = this.getRelative({ x: i + this.movePoint.x % steps, y: origo.y });
+            ctx.fillText(transformer.x.toFixed(2), i + this.movePoint.x % steps, origo.y + 10);
+        }
+        */
+        for (var i = 0; i < this.canvas.width; i += 100) {
+
+        }
+        
         ctx.stroke();      
         
     }
 
     createPoint(data: ISensorPackage): Point {
-        return {x: data.TimeStamp/10, y: data.Value};
+        return {x: data.TimeStamp, y: data.Value};
+    }
+
+    getRelative(p: Point): Point {
+        var moved: Point = { x: p.x - this.movePoint.x, y: this.canvas.height - p.y - this.movePoint.y };
+        var scaled: Point = { x: moved.x / this.scalePoint.x, y: moved.y / this.scalePoint.y };
+        return scaled;
     }
 
     transform(p: Point): Point {
