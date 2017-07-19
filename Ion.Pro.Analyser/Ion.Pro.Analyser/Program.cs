79cab04feb467d7c63f43810f06c47fb3956fc09
Nicolas Fløysvik <nico-floysvik@hotmail.com>
2017-07-19 11:19:27 +0200

    Added real support for telemetry

--- Ion.Pro.Analyser/Ion.Pro.Analyser/Program.cs
+++ Ion.Pro.Analyser/Ion.Pro.Analyser/Program.cs
@@ -65,22 +65,6 @@ namespace Ion.Pro.Analyser
         static void Main(string[] args)
         {
             //manager.Load("../../Data/Sets/126_usart_data.log16");
-            NRFRadio baseRadio = new NRFRadio();
-            baseRadio.Begin();
-            baseRadio.SetPALevel(RF24PaDbm.RF24_PA_HIGH);
-            baseRadio.SetDataRate(RF24Datarate.RF24_250KBPS);
-            baseRadio.SetRetries(0, 0);
-            baseRadio.SetAutoAck(false);
-            baseRadio.OpenReadingPipe(0, "00001");
-            baseRadio.OpenWritingPipe("00001");
-            while (true)
-            {
-                Console.WriteLine("Send");
-                baseRadio.Write(new byte[] { 0x01, 0x99, 00, 00, 10, 151, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 16);
-                System.Threading.Thread.Sleep(1000);
-            }
-            //Console.Read();
-            //return;
             try
             {
                 plinkPath = LegacyPIService.TryFindPlink();
@@ -105,6 +89,7 @@ namespace Ion.Pro.Analyser
                 {
                     Task.Run(() => DataInserter("../../Data/Sets/195_usart_data.log16"));
                 }
+                Task.Run(() => InsertTelemetryData());
                 IonAnalyserWebPage.Run();
             }
             catch (Exception e)
@@ -185,6 +170,61 @@ namespace Ion.Pro.Analyser
             SensorDataStore.GetDefault().ReaderLinker.Add("gpscsv", typeof(GPSDataReader));
             SensorDataStore.GetDefault().ReaderLinker.Add("gpx", typeof(GPXDataReader));
 
+        }
+
+        static void InsertTelemetryData()
+        {
+            Console.WriteLine("Startert listening");
+
+            try
+            {
+                NRFRadio baseRadio = new NRFRadio();
+                Console.WriteLine(baseRadio.Begin());
+                baseRadio.SetPALevel(RF24PaDbm.RF24_PA_HIGH);
+                baseRadio.SetDataRate(RF24Datarate.RF24_250KBPS);
+                baseRadio.SetRetries(0, 0);
+                baseRadio.SetAutoAck(false);
+                baseRadio.OpenReadingPipe(0, "00001");
+                baseRadio.OpenWritingPipe("00001");
+                baseRadio.StartListening();
+                int a = 0;
+                while (true)
+                {
+                    if (!baseRadio.Available())
+                    {
+                        Thread.Yield();
+                    }
+                    while (true)
+                    {
+                        byte[] buffer = new byte[30];
+                        baseRadio.Read(buffer, 30);
+                        if (buffer[0] == 0 && buffer[1] == 0)
+                        {
+                            Thread.Yield();
+                        }
+                        else
+                        {
+                            SensorPackage pack = ParseBytes(buffer);
+                            SensorManager.GetDefault().AddLive("telemetry", pack);
+                            break;
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Failed to open telemetry");
+                Console.WriteLine(e);
+            }
+        }
+
+        static SensorPackage ParseBytes(byte[] bytes)
+        {
+            SensorPackage pack = new SensorPackage();
+            pack.ID = BitConverter.ToUInt16(bytes, 0);
+            pack.Value = BitConverter.ToUInt32(bytes, 2);
+            pack.TimeStamp = BitConverter.ToUInt32(bytes, 6);
+            return pack;
         }
 
         static void InsertSensorTestData()
